d4ce62dd9ed73d556950eee96e00515acafd7a80
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-06-11 08:57:21 -0400

    new variables added. chooseAnswer and confirmAnswer functions added

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,26 @@ const questionEl = document.getElementById('question');
 const optionsEl = document.querySelector('.trivia-options');
 const triviaScoreEl = document.getElementById('trivia-score');
 const totalQuestionEl = document.getElementById('total-question');
+const confirmAnswerBtn = document.getElementById('confirm-answer');
+const playAgainBtn = document.getElementById('play-again');
+const correctAnswerEl = document.getElementById('correct-answer');
 
 let correctAnswer = "", triviaScore = askedCount = 0, totalQuestion = 10;
 
 // Open Trivia DB API
 const triviaAPI = 'https://opentdb.com/api.php?amount=1&difficulty=easy';
+
+// Event listeners
+let eventListeners = () => {
+    confirmAnswerBtn.addEventListener('click', confirmAnswer);
+    playAgainBtn.addEventListener('click', playAgain);
+}
+
+document.addEventListener('click', () => {
+    getQuestion();
+    totalQuestionEl.textContent = totalQuestion;
+    correctAnswerEl.textContent = triviaScore;
+});
 
 // Get question from API
 async function getQuestion() {
@@ -29,5 +44,28 @@ let displayQuestion = (data) => {
             <li> ${index + 1}. <span>${option}</span> </li>
         `).join('')}
     `;
+
+    chooseAnswer();
 };
-getQuestion();
+
+// options selection
+let chooseAnswer = () => {
+    optionsEl.querySelectorAll('li').forEach(function (option) {
+        option.addEventListener('click', function () {
+            if (optionsEl.querySelector('.selected')) {
+                const activeOption = optionsEl.querySelector('.selected');
+                activeOption.classList.remove('selected');
+            }
+            option.classList.add('selected');
+        });
+    });
+}
+
+// Confirm answer
+let confirmAnswer = () => {
+    confirmAnswerBtn.disabled = true;
+    if (optionsEl.querySelector('.selected')) {
+        let selectedAnswer = optionsEl.querySelector('.selected span').textContent;
+        console.log(selectedAnswer);
+    };
+};
