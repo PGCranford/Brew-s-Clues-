cc6db4f5ae03892df6e9b75e3a15a300dfd7fb20
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-06-10 23:42:23 -0400

    expanded on the displayQuestion function and added more variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 // Variables
 const questionEl = document.getElementById('question');
 const optionsEl = document.querySelector('.trivia-options');
+const triviaScoreEl = document.getElementById('trivia-score');
+const totalQuestionEl = document.getElementById('total-question');
+
+let correctAnswer = "", triviaScore = askedCount = 0, totalQuestion = 10;
 
 // Open Trivia DB API
 const triviaAPI = 'https://opentdb.com/api.php?amount=1&difficulty=easy';
@@ -12,8 +16,18 @@ async function getQuestion() {
     displayQuestion(data.results[0]);
 };
 
+// Display question and answer options
 let displayQuestion = (data) => {
-    let correctAnswer = data.correct_answer;
-    console.log(correctAnswer);
-}
+    correctAnswer = data.correct_answer;
+    let incorrectAnswer = data.incorrect_answers;
+    let answerOptions = incorrectAnswer;
+    answerOptions.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer);
+
+    questionEl.innerHTML = `${data.question} <br> <span class="category">${data.category} </span>`;
+    optionsEl.innerHTML = `
+        ${answerOptions.map((option, index) => `
+            <li> ${index + 1}. <span>${option}</span> </li>
+        `).join('')}
+    `;
+};
 getQuestion();
